1a19eecca1c0cc4bfffe8a4675b51e8f59077194
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2019-08-21 22:16:55 -0700

    add real-time message

---
+++ thumbsup/messager/apps.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+
+from django.apps import AppConfig
+
+
+class MessagerConfig(AppConfig):
+    name = 'thumbsup.messager'
+    verbose_name = '消息'
---
+++ thumbsup/messager/urls.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+from django.urls import path
+from thumbsup.messager import views
+
+app_name = 'messager'
+urlpatterns = [
+    path('', views.MessagesListView.as_view(), name='messages_list'),
+    path('send-message/', views.send_message, name='send_message'),
+    path('<username>/', views.ConversationListView.as_view(), name='conversation_detail'),
+]
